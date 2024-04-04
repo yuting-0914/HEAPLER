2f41c2d4e8ce205cb7eaf026a5f7ee048b28482a
chun21729 <152377264+chun21729@users.noreply.github.com>
2024-04-05 01:46:40 +0800

    Update information.php

--- Healper_graph/information.php
+++ Healper_graph/information.php
@@ -1,7 +1,3 @@
-<?php
-header("Content-Type:text/html; charset=UTF-8");
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -31,9 +27,9 @@ header("Content-Type:text/html; charset=UTF-8");
 
                     if($_POST['健保卡號']!=''){
                         $健保卡號=$_POST['健保卡號'];
-                        $sql="SELECT 健保卡號,使用者姓名,身分證字號,病歷號,出生日期,性別,手機號碼,
+                        $sql="SELECT 健保卡號,使用者姓名,身分證字號,病例號,出生日期,性別,手機號碼,
                                         地址,緊急聯絡人姓名,親屬關係,緊急聯絡人電話,緊急聯絡人電子郵件
-                                FROM 使用者 WHERE 健保卡號 = '$健保卡號'";
+                                FROM dbo.使用者 WHERE 健保卡號 = '$健保卡號'";
                     }
                     // else{
                     //     $sql="SELECT * FROM dbo.使用者 ";
@@ -42,26 +38,41 @@ header("Content-Type:text/html; charset=UTF-8");
                     $result = $conn->query($sql);
                     while($row = $result->fetch_assoc()) {
 
-                        echo "
-                        使用者姓名：".$row['使用者姓名']."<br/>
-                        身分證字號：".$row['身分證字號']."<br/>
-                        健保卡號：".$row['健保卡號']."<br/>
-                        病歷號：".$row['病歷號']."<br/>
-                        出生日期：".$row['出生日期']."<br/>
-                        性別：".$row['性別']."<br/>
-                        手機號碼：".$row['手機號碼']."<br/>
-                        地址：".$row['地址']."<br/>
-                        緊急聯絡人姓名：".$row['緊急聯絡人姓名']."<br/>
-                        親屬關係：".$row['親屬關係']."<br/>
-                        緊急聯絡人電話：".$row['緊急聯絡人電話']."<br/>
-                        緊急聯絡人電子郵件：".$row['緊急聯絡人電子郵件']."<br/>";
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>姓名：</th><td>". $row["使用者姓名"]. "</td>";
+                        echo "<th>身分證字號：</th><td>". $row["身分證字號"]. "</td>";
+                        echo "<th>健保卡號：</th><td>". $row["健保卡號"]. "</td>";
+                        echo "<th>出生日期：</th><td>". $row["出生日期"]. "</td>";
+                        echo "<th>性別：</th><td>". $row["性別"]. "</td>";
+                        echo "<th>手機號碼：</th><td>". $row["手機號碼"]. "</td>";
+                        echo "<th>地址：</th><td>". $row["地址"]. "</td>";
+                        echo "<th>緊急聯絡人姓名：</th><td>". $row["緊急聯絡人姓名"]. "</td>";                        
+                        echo "<th>親屬關係：</th><td>". $row["親屬關係"]. "</td>";                        
+                        echo "<th>緊急聯絡人電話：</th><td>". $row["緊急聯絡人電話"]. "</td>";                        
+                        echo "<th>緊急聯絡人電子郵件：</th><td>". $row["緊急聯絡人電子郵件"]. "</td>";
+                        echo "</tr>";
+                        echo "</table>";
+
+                        // echo "
+                        // 使用者姓名：".$row['使用者姓名']."<br/>
+                        // 身分證字號：".$row['身分證字號']."<br/>
+                        // 健保卡號：".$row['健保卡號']."<br/>
+                        // 出生日期：".$row['出生日期']."<br/>
+                        // 性別：".$row['性別']."<br/>
+                        // 手機號碼：".$row['手機號碼']."<br/>
+                        // 地址：".$row['地址']."<br/>
+                        // 緊急聯絡人姓名：".$row['緊急聯絡人姓名']."<br/>
+                        // 親屬關係：".$row['親屬關係']."<br/>
+                        // 緊急聯絡人電話：".$row['緊急聯絡人電話']."<br/>
+                        // 緊急聯絡人電子郵件：".$row['緊急聯絡人電子郵件']."<br/>";
                     }
                 ?>
 
                 <!-- <form name="form" action="http://127.0.0.1:8080/Healper/information_data.php" method="POST" accept-charset="UTF-8" align="center">
                     <input id="modify" name="modify" type="submit" value="修改資料" />
                 </form> -->
-                <a href="information_update.php?modify=true">修改資料</a>
+                <a href="http://127.0.0.1:8080/Healper/information_update.php?modify=true">修改資料</a>
 
             </div> 
         </div>
